27d10fa58f1421e6d9e71f098208ef738265f1df
Gx3 Studios <gabriel.guardiola@estudiantat.upc.edu>
2023-05-03 19:42:57 +0200

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -15,20 +15,22 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-      <h1>Tabla de compotentes y sus FPS/rendimiento</h1>
-      <table className={indexStyles.default}>
-          <tr>
-          <td>Ryzen 7 5700x - RTX2060 - 32GB RAM</td>
-          <td>30FPS estables con bajones</td>
-          <td className={indexStyles.isPlayable}>Jugable</td>
-          </tr>
-      
-          <tr>    
-          <td>I5 9600K - RTX2060 - 16GB</td>
-          <td>20 FPS estables sin bajones</td>
-          <td style="color:red">Injugable</td>
-          </tr>
-      </table>
+      <div className={indexStyles.mainContainer}>
+        <h1>Tabla de compotentes y sus FPS/rendimiento</h1>
+        <table className={indexStyles.default}>
+            <tr>
+            <td>Ryzen 7 5700x - RTX2060 - 32GB RAM</td>
+            <td>30FPS estables con bajones</td>
+            <td className={indexStyles.isPlayable}>Jugable</td>
+            </tr>
+        
+            <tr>    
+            <td>I5 9600K - RTX2060 - 16GB</td>
+            <td>20 FPS estables sin bajones</td>
+            <td style="color:red">Injugable</td>
+            </tr>
+        </table>
+      </div>
       </main>
     </>
   )
